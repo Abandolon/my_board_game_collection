bb155e4c421430260b12bce962d1ed686898e8bc
Anthony Bandolon <abandolon@gmail.com>
2019-09-09 14:57:32 +0200

    Seed File creates Categories and Mechanics from master list provided by Boardgame Atlas website

---
+++ db/mechanics_master.rb
@@ -0,0 +1,366 @@
+@mechanics_master = [
+    {
+      id: "n1GtBt35Rd",
+      name: "Acting"
+    },
+    {
+      id: "PGjmKGi26h",
+      name: "Action / Movement Programming"
+    },
+    {
+      id: "oeg6wN9Eoc",
+      name: "Action Point Allowance System"
+    },
+    {
+      id: "Bc7R8pLoGk",
+      name: "Action Selection"
+    },
+    {
+      id: "05zCZoLvQJ",
+      name: "Area Control"
+    },
+    {
+      id: "3te2oybNR4",
+      name: "Area Enclosure"
+    },
+    {
+      id: "bgGxE0pI2B",
+      name: "Area Movement"
+    },
+    {
+      id: "amcImLdOmD",
+      name: "Asymmetric"
+    },
+    {
+      id: "AZxlPpi5oq",
+      name: "Auction"
+    },
+    {
+      id: "3tuJiW3pps",
+      name: "Betting"
+    },
+    {
+      id: "ZX3hYcF9H7",
+      name: "Bluffing"
+    },
+    {
+      id: "UEzHyBWtz8",
+      name: "Building"
+    },
+    {
+      id: "xuphiSlrxI",
+      name: "Campaign"
+    },
+    {
+      id: "iWODHwRGuU",
+      name: "Card Drafting"
+    },
+    {
+      id: "ebJKldFVeS",
+      name: "Card Game"
+    },
+    {
+      id: "ngCSHHk0H2",
+      name: "Card Placement"
+    },
+    {
+      id: "Gan96fffLL",
+      name: "Chit-Pull System"
+    },
+    {
+      id: "K0ykGhTMa8",
+      name: "Comodity Speculation"
+    },
+    {
+      id: "AAi6B8NLaV",
+      name: "Confusion"
+    },
+    {
+      id: "hwyCo1W0hi",
+      name: "Conversation"
+    },
+    {
+      id: "33UT4gTFqy",
+      name: "Cooperative"
+    },
+    {
+      id: "9mNukNBxfZ",
+      name: "Cooperative Play"
+    },
+    {
+      id: "xVDf5dmJts",
+      name: "Crayon Rail System"
+    },
+    {
+      id: "kZf6CVjtq7",
+      name: "D6 RPG"
+    },
+    {
+      id: "vZsDDAdOoe",
+      name: "Deck Building"
+    },
+    {
+      id: "GsNGxZFNCK",
+      name: "Deduction"
+    },
+    {
+      id: "5kvyChnWuO",
+      name: "Dexterity"
+    },
+    {
+      id: "lVSHu9efHv",
+      name: "Dice Building"
+    },
+    {
+      id: "R0bGq4cAl4",
+      name: "Dice Rolling"
+    },
+    {
+      id: "wV5peB05xs",
+      name: "Drafting"
+    },
+    {
+      id: "KfTS5BwIsu",
+      name: "Drawing"
+    },
+    {
+      id: "Ouo00D4ka7",
+      name: "Drinking"
+    },
+    {
+      id: "yu3eas6v7A",
+      name: "Engine Building"
+    },
+    {
+      id: "hUn7uJHrYm",
+      name: "Euro Game"
+    },
+    {
+      id: "kS8npG0jl8",
+      name: "Fighting"
+    },
+    {
+      id: "S0O9ucexPS",
+      name: "Flicking"
+    },
+    {
+      id: "qu5BcGjAzk",
+      name: "Grid Movement"
+    },
+    {
+      id: "WPytek5P8l",
+      name: "Hand Management"
+    },
+    {
+      id: "i3xnwJ7VPV",
+      name: "Hand-Eye Coordination"
+    },
+    {
+      id: "6CyVyXRn1C",
+      name: "Hex and Counter"
+    },
+    {
+      id: "aQZ40lKv8O",
+      name: "Hidden Movement"
+    },
+    {
+      id: "Voqy2dgrIM",
+      name: "Hidden Traitor"
+    },
+    {
+      id: "DwmsVEvNVd",
+      name: "Legacy"
+    },
+    {
+      id: "MaXzmoZUoX",
+      name: "Line Drawing"
+    },
+    {
+      id: "UaLql58fua",
+      name: "Live action and Role Playing game (Larp)"
+    },
+    {
+      id: "ar56ACXDNe",
+      name: "Mancala"
+    },
+    {
+      id: "r6yIFvyXDD",
+      name: "Memory"
+    },
+    {
+      id: "U3zhCQH7Et",
+      name: "Modular Board"
+    },
+    {
+      id: "UHdPUeuqyZ",
+      name: "Paper and Pencil"
+    },
+    {
+      id: "GNtouC8NLm",
+      name: "Partnerships"
+    },
+    {
+      id: "3dFd0RwsY4",
+      name: "Party Game"
+    },
+    {
+      id: "9YdRn9J9oZ",
+      name: "Pattern Building"
+    },
+    {
+      id: "uZJS07nXF5",
+      name: "Pattern Recognition"
+    },
+    {
+      id: "BbTMRkwL0b",
+      name: "Pick-up and Deliver"
+    },
+    {
+      id: "BGrhzIN69D",
+      name: "Player Elimination"
+    },
+    {
+      id: "YyBg5CzHBF",
+      name: "Player vs. Player"
+    },
+    {
+      id: "MEAoOygZsA",
+      name: "Point to Point Movement"
+    },
+    {
+      id: "ea1eaPBQn8",
+      name: "Pool Building"
+    },
+    {
+      id: "hmipYN1R1I",
+      name: "Press Your Luck"
+    },
+    {
+      id: "24FWssBC3o",
+      name: "Puzzle"
+    },
+    {
+      id: "E9VKQ8uMSP",
+      name: "Real Time"
+    },
+    {
+      id: "Rt6V388y6M",
+      name: "Resource Gathering"
+    },
+    {
+      id: "j7x8jY4ay2",
+      name: "Rock-Paper-Scissors"
+    },
+    {
+      id: "EVeAdboGUA",
+      name: "Role Playing"
+    },
+    {
+      id: "mGBzR68m8Z",
+      name: "Roll / Spin and Move"
+    },
+    {
+      id: "zIPRS41oiN",
+      name: "Roll and Write"
+    },
+    {
+      id: "FA6HjbhdNW",
+      name: "Rondel"
+    },
+    {
+      id: "asw8k7EIJI",
+      name: "Route Building"
+    },
+    {
+      id: "c6gkRM7rSy",
+      name: "Route/Network Building"
+    },
+    {
+      id: "RwgEONzIzc",
+      name: "Secret Unit Deployment"
+    },
+    {
+      id: "lA3KUtVFCy",
+      name: "Set Collection"
+    },
+    {
+      id: "eRe1jJCBFe",
+      name: "Simulation"
+    },
+    {
+      id: "DEvPj5twid",
+      name: "Simultaneous Play"
+    },
+    {
+      id: "za4PP1LH00",
+      name: "Simultaneous action selection"
+    },
+    {
+      id: "kuRNhFADjS",
+      name: "Skirmish"
+    },
+    {
+      id: "x3wVCq1HEP",
+      name: "Social Deduction"
+    },
+    {
+      id: "jbLrZb1xIb",
+      name: "Stock Holding"
+    },
+    {
+      id: "GUoWg3Mfh5",
+      name: "Storytelling"
+    },
+    {
+      id: "yDlgk7rXno",
+      name: "Tactical Movement"
+    },
+    {
+      id: "jCRze30VP1",
+      name: "Tactics"
+    },
+    {
+      id: "T8JEFYwoqy",
+      name: "Take That"
+    },
+    {
+      id: "9jnCsVuRat",
+      name: "Teams"
+    },
+    {
+      id: "8PN2HE86wg",
+      name: "Tile Placement"
+    },
+    {
+      id: "cXd5KaXXZo",
+      name: "Time Track"
+    },
+    {
+      id: "AVY6EvSQTP",
+      name: "Trading"
+    },
+    {
+      id: "3GSQl800lk",
+      name: "Trick-taking"
+    },
+    {
+      id: "zzsE4jtI1b",
+      name: "Variable Phase Order"
+    },
+    {
+      id: "XM2FYZmBHH",
+      name: "Variable Player Powers"
+    },
+    {
+      id: "JYYdBW6UCE",
+      name: "Voting"
+    },
+    {
+      id: "yomuPpKFx4",
+      name: "Wargame"
+    },
+    {
+      id: "fBOTEBUAmV",
+      name: "Worker Placement"
+    }
+  ]
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,44 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require_relative 'mechanics_master.rb'
+require_relative 'categories_master.rb'
+
+puts "Resetting all Boardgame Mechanics"
+Boardgamesmechanic.destroy_all
+puts "Successfully reset Boardgame Mechanic references"
+
+puts "Resetting all Boardgame Categories"
+Boardgamescategory.destroy_all
+puts "Successfully reset Boardgame Categories references"
+
+
+puts "Resetting All Mechanics"
+
+Mechanic.destroy_all
+
+puts "Resetting All Categories"
+
+Category.destroy_all
+
+# Create Mechanics
+puts "Creating Mechanics"
+
+@mechanics_master.each do |mechanic|
+  Mechanic.create(name: mechanic[:name], bga_id: mechanic[:id])
+  puts "#{mechanic[:name]} has been created"
+end
+
+puts "Completed. Successfully created #{Mechanic.count} mechanics"
+
+# Create Categories
+
+puts "Creating Categories"
+
+@categories_master.each do |category|
+  Category.create(name: category[:name], bga_id: category[:id])
+  puts "#{category[:name]} has been created"
+
+end
+
+puts "Completed. Successfully created #{Category.count} categories"
