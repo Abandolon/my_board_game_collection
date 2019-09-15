dd1be8a7e90919aa1278560459f34ca245991cec
Anthony Bandolon <abandolon@gmail.com>
2019-09-15 13:06:56 +0200

    [WIP] COnfigure Mailer, need to set up Domain, Email

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,24 @@
+class UserMailer < ApplicationMailer
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.user_mailer.welcome.subject
+  #
+  def welcome
+    @greeting = "Hi"
+
+    mail to: "to@example.org"
+  end
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.user_mailer.request.subject
+  #
+  def request
+    @greeting = "Hi"
+
+    mail to: "to@example.org"
+  end
+end
