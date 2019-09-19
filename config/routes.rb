c6cdd0150b8edab3f1375021e232b288be75d844
Anthony Bandolon <abandolon@gmail.com>
2019-09-19 17:20:53 +0200

    Update routing, Update Boardgame Controller and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { registrations: "registrations" }
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :profiles, only: [:new, :create, :show]
+  resources :profiles, only: [:new, :create, :show, :edit, :update]
   resources :boardgames, only: [:index, :show, :create, :destroy]
   post 'boardgames/search', action: :search, controller: 'boardgames'
 end
