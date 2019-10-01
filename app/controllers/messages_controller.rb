8ee831d5ade0c69c173e2b0c61edc8f6d08bc493
Anthony Bandolon <abandolon@gmail.com>
2019-10-02 01:32:27 +0200

    [WIP] Conversation Index styling. Need to Style boardgame#show, message#index, conversation#new

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -5,7 +5,6 @@ class MessagesController < ApplicationController
   def create
     receipt = current_user.reply_to_conversation(@conversation, params[:body])
     redirect_to conversation_path(receipt.conversation)
-
   end
 
   private
