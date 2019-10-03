2e63132f7c120dcc72c31fc4952db0676b7e9b3b
Anthony Bandolon <abandolon@gmail.com>
2019-10-03 22:41:02 +0200

    Add validation to avoid duplicate Usersboardgame objects

--- app/models/usersboardgame.rb
+++ app/models/usersboardgame.rb
@@ -1,4 +1,6 @@
 class Usersboardgame < ApplicationRecord
   belongs_to :boardgame
   belongs_to :user
+
+  validates_uniqueness_of :boardgame_id, scope: :user_id, message: "You've already added this game"
 end
