ed42b9e8be30228a53aef4b47c32ce6f1b41c95d
Anthony Bandolon <abandolon@gmail.com>
2019-11-25 11:14:44 +0100

    updated Gems

--- app/models/usersboardgame.rb
+++ app/models/usersboardgame.rb
@@ -2,5 +2,6 @@ class Usersboardgame < ApplicationRecord
   belongs_to :boardgame
   belongs_to :user
 
-  validates_uniqueness_of :boardgame_id, scope: :user_id, message: "You've already added this game"
+  # validates_uniqueness_of :boardgame_id, scope: :user_id, message: "You've already added this game"
+  validates_uniqueness_of :user_id, :scope => [:boardgame_id]
 end
