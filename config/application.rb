977ef82fb889dffc11e76d4198aad84db0da4c23
Anthony Bandolon <anthony@bandolon.me>
2022-01-28 15:11:58 +0100

    update

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,6 @@
-require_relative 'boot'
+require_relative "boot"
 
-require 'rails/all'
+require "rails/all"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -8,22 +8,15 @@ Bundler.require(*Rails.groups)
 
 module MyBoardGameCollection
   class Application < Rails::Application
-    config.generators do |generate|
-      generate.assets false
-      generate.helper false
-      generate.test_framework :test_unit, fixture: false
-    end
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
-
-    config.action_mailer.delivery_method = :postmark
-    config.action_mailer.postmark_settings = { :api_token =>
-      ENV['POSTMARK_API'] }
+    # Configuration for the application, engines, and railties goes here.
+    #
+    # These settings can be overridden in specific environments using the files
+    # in config/environments, which are processed later.
+    #
+    # config.time_zone = "Central Time (US & Canada)"
+    # config.eager_load_paths << Rails.root.join("extras")
   end
-
 end
